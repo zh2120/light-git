2fb971b33c8cb4e49f80f8fe0acbac66e52bbf7c
traveller <931992120@qq.com>
2018-02-10 19:45:08 +0800

    editor

--- js/views/repos/File.js
+++ js/views/repos/File.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Text, ScrollView, StyleSheet, WebView } from 'react-native'
-import { Loading } from '../../components'
-import {  bindActions } from '../../reducers/comReducer'
-import { fileContent, getFileDenied } from '../../reducers/repoReducer'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {Text, ScrollView, StyleSheet, WebView} from 'react-native'
+import {Loading} from '../../components'
+import {bindActions} from '../../reducers/comReducer'
+import {fileContent, getFileDenied} from '../../reducers/repoReducer'
 
-export default connect(({ repoInfo }) => ({ file: repoInfo.file }), bindActions({
+export default connect(({repoInfo}) => ({file: repoInfo.file}), bindActions({
     fileContent,
     getFileDenied
 }))(
     class extends Component {
-        static navigationOptions = ({ navigation }) => {
+        static navigationOptions = ({navigation}) => {
             const params = navigation.state.params;
             return {
                 headerTitle: params && params.path,
@@ -27,13 +27,13 @@ export default connect(({ repoInfo }) => ({ file: repoInfo.file }), bindActions(
         }
 
         componentDidMount() {
-            const { navigation, fileContent } = this.props;
+            const {navigation, fileContent} = this.props;
             // console.log('componentDidMount')
 
             if (navigation.state.params) {
-                const { fullName, path } = navigation.state.params;
+                const {fullName, path} = navigation.state.params;
 
-                fileContent({ fullName, path })
+                fileContent({fullName, path})
             }
         }
 
@@ -44,22 +44,25 @@ export default connect(({ repoInfo }) => ({ file: repoInfo.file }), bindActions(
         postMessage = (code) => {
             if (this.web) {
                 // console.log('postMessage', code)
-                this.web.postMessage(JSON.stringify({ cmd: 'code', code }))
+                this.web.postMessage(JSON.stringify({cmd: 'code', code}))
             }
         };
 
         render() {
-            const { file } = this.props;
+            const {file} = this.props;
 
             // console.log('render')
             if (isEmpty(file)) return <Loading/>;
-
+            const source = ios ? require('../../editor/index.html') : {
+                html: this.props.html,
+                baseUrl: 'file:///android_asset/web/'
+            };
             return (
                 <WebView
                     onLoad={() => this.postMessage(file)}
-                    style={{ flex: 1 }}
+                    style={{flex: 1}}
                     ref={web => this.web = web}
-                    source={require('../../editor/index.html')}
+                    source={source}
                 />
             )
         }
@@ -67,7 +70,7 @@ export default connect(({ repoInfo }) => ({ file: repoInfo.file }), bindActions(
 )
 
 const styles = StyleSheet.create({
-    wrap: { padding: 8 },
-    fileText: { fontSize: 14, fontFamily: 'Arial', lineHeight: 22, color: '#333' }
+    wrap: {padding: 8},
+    fileText: {fontSize: 14, fontFamily: 'Arial', lineHeight: 22, color: '#333'}
 });
 
